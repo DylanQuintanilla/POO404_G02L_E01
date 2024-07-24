d6a2363a26f62f60db08b5ae1d8cc65d937761d1
DylanQuintanilla <dylanalfonsoq18@gmail.com>
2024-07-24 13:52:15 -0600

    Arreglo de main
    Error y se subio una guia aqui sin querer

--- MG220754_guia_01/src/guia1/guia1.java
+++
@@ -1,7 +0,0 @@
-package guia1;
-
-public class guia1 {
-    public static void main(String[] args){
-        System.out.println("Hola mundo");
-    }
-}
--- MG220754_guia_01/src/usoparametros/LecturaParametros.java
+++
@@ -1,4 +0,0 @@
-package usoparametros;
-
-public class LecturaParametros {
-}
