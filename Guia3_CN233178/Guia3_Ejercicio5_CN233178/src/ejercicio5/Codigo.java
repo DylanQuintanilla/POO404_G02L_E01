75b6940f3691460b033563c82ea6a77bbf7a1678
Robert1234567-bit <robertoantonioc072@gmail.com>
2024-07-24 19:00:47 -0600

    Add files via upload

---
+++ Guia3_CN233178/Guia3_Ejercicio5_CN233178/src/ejercicio5/Codigo.java
@@ -0,0 +1,23 @@
+package ejercicio5;
+
+public class Codigo {
+
+    class Clase {
+        static int contador;
+        Clase() { //Constructor
+            contador++;
+        }
+        int getContador() { //Obtenemos el valor de contador
+            return contador;
+        }
+    }
+    public class Codigo {
+        public static void main(String[] args) {
+            Clase uno = new Clase();
+            Clase dos = new Clase();
+            Clase tres = new Clase();
+            Clase cuatro = new Clase();
+            System.out.println("Hemos declarado " + dos.getContador() + " objetos.");
+        }
+    }
+}
